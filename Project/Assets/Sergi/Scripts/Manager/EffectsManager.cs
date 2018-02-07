cb95ba34a527550ada52b7065af00aeba428e81a
Sergi van Ravenswaay <contact@ikbensergi.com>
2018-02-07 19:01:51 +0100

    Environment update and Tween init

---
+++ Project/Assets/Sergi/Scripts/Manager/EffectsManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsManager : MonoBehaviour {
+    public Day_State DayState;
+    public Environment_State EnvState;
+    //Day States
+    public void Change_Day() {
+        switch (DayState) {
+            case (Environment_State.Morning):
+                //DO UNHEALTHY STUFF
+                break;
+            case (Environment_State.Noon):
+                //DO NEUTRAL STUFF
+                break;
+            case (Environment_State.Evening):
+                //DO HEALTHY STUFF
+                break;
+        }
+    }
+
+    //Environment states
+    public void Change_Environment() {
+        switch(EnvState) {
+            case (Environment_State.Unhealthy):
+                //DO UNHEALTHY STUFF
+                break;
+            case (Environment_State.Neutral):
+                //DO NEUTRAL STUFF
+                break;
+            case (Environment_State.Healthy):
+                //DO HEALTHY STUFF
+                break;
+        }
+    }
+}
+
+public enum Environment_State {
+    Unhealthy,
+    Neutral,
+    Healthy
+}
+
+public enum Day_State {
+    Morning,
+    Noon,
+    Evening
+}

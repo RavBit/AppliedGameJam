b251dec89864b6faa9d2464e8595fc6a57e73f6d
Vincent <vincentmc99@hotmail.com>
2018-02-07 14:22:22 +0100

    ResourceManager
    resource manager can be found in Vincent/Scripts

---
+++ Project/Assets/Vincent/Scripts/ResourceManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum Resources { population, currency, happiness, environment}
+
+public class ResourceManager : MonoBehaviour {
+
+	public static ResourceManager instance;
+	private int population, currency, happiness, environment;
+
+	//Properties for the four resources to enable function calls upon change. 
+
+	/*
+	 * ///////////////////////////////////////////////////////////////////////////////////
+	 * ///																			   ///
+	 * !!! The only place in this script that needs to be changed are these properties.!!!
+	 * ///																			   ///
+	 * ///////////////////////////////////////////////////////////////////////////////////
+	 */
+
+	private int Population {
+		get {
+			return population;
+		}
+		set {
+			population += value;
+			Debug.Log("Adding to population in property!");
+			//Some other update functions that need to be called upon changing this value.
+		}
+	}
+	private int Currency {
+		get {
+			return currency;
+		}
+		set {
+			currency += value;
+			Debug.Log("Adding to currency in property!");
+			//Some other update functions that need to be called upon changing this value.
+		}
+	}
+	private int Happiness {
+		get {
+			return happiness;
+		}
+		set {
+			happiness += value;
+			Debug.Log("Adding to happiness in property!");
+			//Some other update functions that need to be called upon changing this value.
+		}
+	}
+	private int Environment {
+		get {
+			return environment;
+		}
+		set {
+			environment += value;
+			Debug.Log("Adding to environment in property!");
+			//Some other update functions that need to be called upon changing this value.
+		}
+	}
+
+	private void Awake() {
+		//Lazy singleton init
+		if(instance == null)
+			instance = this;
+		else {
+			Debug.Log("More than one resource manager in scene, destroying this.");
+			DestroyImmediate(this);
+		}
+
+	}
+
+	private void Start() {
+		SendResourceMessage(new ResourceMessage(Resources.currency, 10), new ResourceMessage(Resources.happiness, 50), new ResourceMessage(Resources.environment, 30), new ResourceMessage(Resources.population, 100));
+	}
+
+	//This function is made to handle all resource change subjects. It accepts both positive and negative values.
+	//It also accepts an infinite amount of changes per function call.
+	//This is the main communication reciever meant for the rest of the game to have influence on the resources.
+	public void SendResourceMessage(params ResourceMessage[] res) {
+		foreach(ResourceMessage i in res) {
+			Resources temp = i.GetResourceType();
+			//Debug.Log(temp);
+			//Debug.Log(i.GetResourceType());
+			switch(temp) {
+				case Resources.population:
+					Population += i.GetAmount();
+					Debug.Log("pop" + i.GetAmount());
+					break;
+				case Resources.currency:
+					Currency += i.GetAmount();
+					Debug.Log("cur" + i.GetAmount());
+					break;
+				case Resources.happiness:
+					Happiness += i.GetAmount();
+					Debug.Log("hap" + i.GetAmount());
+					break;
+				case Resources.environment:
+					Environment += i.GetAmount();
+					Debug.Log("env" + i.GetAmount());
+					break;
+				default:
+					Debug.Log("Unhandled enum type");
+					break;
+			}
+		}
+	}
+}
+
+public class ResourceMessage {
+	private Resources resourceType;
+	private int amount;
+	public ResourceMessage(Resources t, int i) {
+		resourceType = t;
+		amount = i;
+	}
+
+	public Resources GetResourceType() {
+		return resourceType;
+	}
+
+	public int GetAmount() {
+		return amount;
+	}
+}

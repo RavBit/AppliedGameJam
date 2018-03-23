c0cb106193eec455d31b46662c610cc88bb6bfe1
Sergi van Ravenswaay <sergi@live.nl>
2018-03-23 15:07:10 +0100

    breaking news fixes

--- Project/Assets/Main/Scripts/NewsManager.cs
+++ Project/Assets/Main/Scripts/NewsManager.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class NewsManager : MonoBehaviour {
-
+    public GameObject scrollitem;
+    public Transform startpos;
+    public bool scrolling = true;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        EventManager.SendBreakingNews += StartScrol;
+    }
+    private void StartScrol(string s)
+    {
+        s = "";
+        scrolling = true;
+        StartCoroutine("Scrollnews");
+    }
+    public IEnumerator Scrollnews()
+    {
+        while (scrolling)
+        {
+            scrollitem.transform.DOLocalMoveX(-15, 15);
+            yield return new WaitForSeconds(15);
+            scrollitem.transform.localPosition = new Vector3(50, 0, 0);
+        }
+
+        scrollitem.transform.position = startpos.position;
+    }
+
+    public void StopScrolling()
+    {
+        scrolling = false;
+        StopCoroutine("Scrollnews");
+    }
 }

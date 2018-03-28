692feee613a9ab5e0764739c820a3965b495df40
Sergi van Ravenswaay <sergi@live.nl>
2018-03-28 22:37:42 +0200

    last changes

--- Project/Assets/Main/Scripts/GoToSite.cs
+++ Project/Assets/Main/Scripts/GoToSite.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoToSite : MonoBehaviour {
 
 	public void LoadSite(string url) {
 		Application.OpenURL(url);
 	}
+
+    public void LoadStart()
+    {
+        SceneManager.LoadScene(0);
+    }
 }

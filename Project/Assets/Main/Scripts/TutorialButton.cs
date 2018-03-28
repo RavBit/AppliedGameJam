a38a92d0cd22181db3880da011dd2a31f5b74430
Vincent <vincentmc99@hotmail.com>
2018-03-28 18:18:01 +0200

    tutorial scene playbuttons

---
+++ Project/Assets/Main/Scripts/TutorialButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialButton : MonoBehaviour {
+
+	public Button playbutton;
+	public int delay = 10;
+
+	private void Start() {
+		StartCoroutine(ButtonEnable());
+	}
+
+	private IEnumerator ButtonEnable() {
+		playbutton.interactable = false;
+		yield return new WaitForSeconds(delay);
+		playbutton.interactable = true;
+	}
+}
